b6bf695d4b0137f50d2567f45b8c749459bbeb52
Alex Pimonov <you.pimonov2@gmail.com>
2016-11-23 14:57:02 +0200

    Finish static pages

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'sample_app/home'
+
+  get 'sample_app/help'
+  get 'sample_app/about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
